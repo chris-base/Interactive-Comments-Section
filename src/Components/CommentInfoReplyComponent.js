b38f79c2419b1e9bfb26e9f0b0f0adc7fe4cf47f
chris-base <baseappsdev@gmail.com>
2022-06-13 15:30:58 -0500

    checks comments if curr user/sets layouts

--- src/Components/CommentInfoReplyComponent.js
+++ src/Components/CommentInfoReplyComponent.js
@@ -4,6 +4,8 @@ import juliusomo from "./assets/avatars/image-juliusomo.png";
 import maxblagun from "./assets/avatars/image-maxblagun.png";
 import ramsesmiron from "./assets/avatars/image-ramsesmiron.png";
 import replyImg from "./assets/icon-reply.svg";
+import deleteImg from "./assets/icon-delete.svg";
+import editImg from "./assets/icon-edit.svg";
 
 const images = {
   amyrobson: amyrobson,
@@ -18,14 +20,37 @@ const CommentInfoReplyComponent = ({ commentUser, commentDate }) => {
       <div id='commentInfoContainer'>
         <div id='userImg' style={{ backgroundImage: "url(" + images[`${commentUser}`] + ")" }}></div>
         <p id='userName'>{commentUser}</p>
+
+        {commentUser === "juliusomo" ? (
+          <div id='currUserTagContainer'>
+            <p id='currUserTagText'>you</p>
+          </div>
+        ) : (
+          <></>
+        )}
+
         <p id='commentTime'>{commentDate}</p>
       </div>
 
       <div id='replyContainer'>
-        <button id='replyButtonContainer'>
-          <div id='replyImg' style={{ backgroundImage: "url(" + replyImg + ")" }}></div>
-          <p id='replyText'>Reply</p>
-        </button>
+        {commentUser === "juliusomo" ? (
+          <div id='userDeleteEditContainer'>
+            <div id='deleteButton'>
+              <div id='deleteImg' style={{ backgroundImage: "url(" + deleteImg + ")" }} />
+              <p id='deleteText'>Delete</p>
+            </div>
+
+            <div id='editButton'>
+              <div id='editImg' style={{ backgroundImage: "url(" + editImg + ")" }} />
+              <p id='editText'>Edit</p>
+            </div>
+          </div>
+        ) : (
+          <div id='replyButtonContainer'>
+            <div id='replyImg' style={{ backgroundImage: "url(" + replyImg + ")" }}></div>
+            <p id='replyText'>Reply</p>
+          </div>
+        )}
       </div>
     </div>
   );
